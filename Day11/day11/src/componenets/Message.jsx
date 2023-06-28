62275de4a674196263d0e077e968e11b9a01b7db
Bhavdeep <bhavdeepkaushal392@gmail.com>
2023-06-28 07:39:26 +0530

    day11 version2

--- Day11/day11/src/componenets/Message.jsx
+++ Day11/day11/src/componenets/Message.jsx
@@ -34,19 +34,23 @@ const Message = ({message}) => {
 
   useEffect(()=>{
     ref.current?.scrollIntoView({behavior: "smooth"})
-  },[message])
+  },[message]);
 
-  
+  // TimeStamp
+  const date = message?.date?.seconds ? new Date(message.date.seconds * 1000) : null;
+  const formattedTime = date ? date.toLocaleTimeString() : null;
+
   return (
     <div ref={ref} className={`message ${message.senderId==currentUser.uid && "owner"}`}>
       <div className="messageInfo">
         <img src="https://images.unsplash.com/photo-1687561114607-4828b9997897?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=374&q=80" alt="" />
-        <span>just now</span>
+        <span>{formattedTime}</span>
       </div>
       <div className="messageContent">
           <p>{message.text}</p>
       </div>
     </div>
+ 
   )
 }
 
